c569b92dac0ad3e1d072eafbf1370ee99043a2bc
Niels <ncmgmoss@student.avans.nl>
2016-04-24 23:26:21 +0200

    Added option to join a game and get the players in a game.

--- app/js/controllers/gameCtrl.js
+++ app/js/controllers/gameCtrl.js
@@ -1,56 +1,86 @@
-module.exports = function($scope, GameService, $localStorage) {
-  console.log('gameCtrl');
-  var self = this;
+module.exports = function ($scope, GameService, $localStorage) {
+    console.log('gameCtrl');
+    var self = this;
 
-  self.toggled;
+    self.toggled;
 
-  self.games = [];
-  $scope.test = "Testje in de controllers";
+    self.games = [];
+    $scope.test = "Testje in de controllers";
 
-  self.toggleClass = function(){
-    self.toggled = !self.toggled;
-  }
-
-  GameService.getAllGames({
-    onSuccess: function(result){
-      // TODO: not use this.games in here, this creates a different scope
-      //console.log(result);
-      angular.forEach(result.data, function(value, key) {
-        self.games[key] = value;
-      })
-      //self.games.push(result.data[0]);
-      //this.games = result.data;
-    },
-    onError: function(err){
-      console.log(err);
+    self.toggleClass = function () {
+        self.toggled = !self.toggled;
     }
-  })
 
-  self.createGame = function(newGame){
-    console.log(newGame);
-    GameService.createGame(JSON.stringify(newGame), {
-      onSuccess: function(result){
-        console.log(result.data);
-        alert('Successfully added game with ID ' +  result.data._id);
-      },
-      onError: function(err){
-        console.log(err);
-      }
-    })
-  }
-
-  self.getMyGames = function(){
-    var query = '?createdBy=' + $localStorage.username;
-    GameService.getMyGames(query, {
-      onSuccess: function(result){
-        angular.forEach(result.data, function(value, key) {
-          self.games[key] = value;
+    self.getAllGames = function() {
+        GameService.getAllGames({
+            onSuccess: function (result) {
+                // TODO: not use this.games in here, this creates a different scope
+                //console.log(result);
+                angular.forEach(result.data, function (value, key) {
+                    self.games[key] = value;
+                })
+                //self.games.push(result.data[0]);
+                //this.games = result.data;
+            },
+            onError: function (err) {
+                console.log(err);
+            }
         })
-        console.log(result.data);
-      },
-      onError: function(err){
-        console.log(err);
-      }
-    })
-  }
+    }
+
+    self.getAllGames();
+
+    self.createGame = function (newGame) {
+        console.log(newGame);
+        GameService.createGame(JSON.stringify(newGame), {
+            onSuccess: function (result) {
+                console.log(result.data);
+                alert('Successfully added game with ID ' + result.data._id);
+            },
+            onError: function (err) {
+                console.log(err);
+            }
+        })
+    }
+
+    self.getMyGames = function () {
+        var query = '?createdBy=' + $localStorage.username;
+        GameService.getMyGames(query, {
+            onSuccess: function (result) {
+                angular.forEach(result.data, function (value, key) {
+                    self.games[key] = value;
+                })
+                console.log(result.data);
+            },
+            onError: function (err) {
+                console.log(err);
+            }
+        })
+    }
+
+    self.joinGame = function (id) {
+        GameService.joinGame(id, {
+            onSuccess: function (result) {
+                self.getAllGames();
+            },
+            onError: function (err) {
+                alert(err.data.message);
+            }
+        })
+    }
+
+    self.getPlayers = function (id) {
+        GameService.getPlayers(id, {
+            onSuccess: function (result) {
+                var names = [];
+                angular.forEach(result.data, function (value, key) {
+                    names.push(value.name);
+                })
+                alert(names);
+            },
+            onError: function (err) {
+                console.log(err);
+            }
+        })
+    }
 }
