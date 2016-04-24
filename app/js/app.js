03f8175000ec79d37642b18f0e7d6940fe46ebbc
Niels <ncmgmoss@student.avans.nl>
2016-04-24 13:31:55 +0200

    Included all ui-router-extras modules

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,17 @@
 require('angular/angular');
 require('ng-storage/ngStorage.min');
-require('angular-ui-router/release/angular-ui-router');
 
+require('angular-ui-router/release/angular-ui-router');
+require('ui-router-extras/release/modular/ct-ui-router-extras.core');
+require('ui-router-extras/release/modular/ct-ui-router-extras.sticky');
+require('ui-router-extras/release/modular/ct-ui-router-extras.dsr');
+require('ui-router-extras/release/modular/ct-ui-router-extras.future');
+require('ui-router-extras/release/modular/ct-ui-router-extras.previous');
+require('ui-router-extras/release/modular/ct-ui-router-extras.statevis');
+require('ui-router-extras/release/modular/ct-ui-router-extras.transition');
 
 // Create your app
-var app = angular.module('app', ['ui.router', 'ngStorage']);
+var app = angular.module('app', ['ui.router', 'ngStorage', 'ct.ui.router.extras.sticky', 'ct.ui.router.extras.dsr', 'ct.ui.router.extras.future', 'ct.ui.router.extras.previous', 'ct.ui.router.extras.statevis', 'ct.ui.router.extras.transition']);
 
 // Routing
 require('./config/router.js')(app)
